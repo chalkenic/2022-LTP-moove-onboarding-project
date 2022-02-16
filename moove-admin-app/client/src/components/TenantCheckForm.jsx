e4022390895a1f1e1835fb64e4d280de9eb2ca31
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-02-16 20:56:45 +0000

    Created initial form react file for tenancy checks

---
+++ moove-admin-app/client/src/components/TenantCheckForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+      <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
+      <input type="url" placeholder="Driving License/Passport" {...register("Driving License/Passport", {required: true})} />
+      <input type="url" placeholder="Student ID" {...register("Student ID", {})} />
+
+      <input type="submit" />
+    </form>
+  );
+}
