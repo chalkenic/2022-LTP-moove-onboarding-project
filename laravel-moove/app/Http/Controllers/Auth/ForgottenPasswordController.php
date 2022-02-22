e3efd7122261eed22e2efc1a0f22fd260f0dbfd7
Luke Morcom <luke@morcom.io>
2022-02-22 23:04:34 +0000

    Extract password reset to dedicated controllers. Customize email message.

---
+++ laravel-moove/app/Http/Controllers/Auth/ForgottenPasswordController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgottenPasswordController extends Controller
+{
+    public function __construct() {
+        $this->middleware(['guest']);
+    }
+
+    public function index() {
+        return view('auth.forgot-password');
+    }
+
+    public function store(Request $request) {
+        $request->validate(['email' => 'required|email']);
+        
+        Password::sendResetLink((
+            $request->only('email')
+        ));
+
+        return back()->with('status', 'If there is a moove account with that email, we just sent reset instructions to it.');
+    }
+}
--- laravel-moove/routes/web.php
+++ laravel-moove/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgottenPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Tenant\TenantController;
 use Illuminate\Auth\Events\PasswordReset;
@@ -18,55 +20,15 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+Route::get('/forgot-password', [ForgottenPasswordController::class, 'index'])->name('password.request');
+Route::post('/forgot-password', [ForgottenPasswordController::class, 'store'])->name('password.email');
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'index'])->name('password.reset');
+Route::post('/reset-password', [ResetPasswordController::class, 'store'])->middleware('guest')->name('password.update');
 
 // General routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Tenant routes
 Route::get('/tenant-home', [TenantController::class, 'index'])->name('tenant-home');
-
-Route::get('/forgot-password', function () {
-    return view('auth.forgot-password');
-})->middleware('guest')->name('password.request');
-
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
-
-    Password::sendResetLink(
-        $request->only('email')
-    );
-
-    return back()->with('status', 'If that email is associated with an account, we just sent you reset instructions.');
-
-})->middleware('guest')->name('password.email');
-
-Route::get('/reset-password/{token}', function ($token) {
-    return view('auth.reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
-Route::post('/reset-password', function(Request $request) {
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|confirmed'
-    ]);
-
-    $status = Password::reset(
-        $request->only('email', 'password', 'password_confirmation', 'token'),
-        function ($user, $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
-
-            $user->save();
-
-            event(new PasswordReset($user));
-        }
-    );
-
-    return $status === Password::PASSWORD_RESET
-    ? redirect()->route('login')->with('status', __($status))
-    : back()->withErrors(['email' => [__($status)]]);
-})->middleware('guest')->name('password.update');
 
 // Admin routes
