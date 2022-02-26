b9491cfbebc13c694cd88d0474fdb539472cbe37
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-02-26 17:57:13 +0000

    Created new page for tenancy form

---
+++ moove-admin-app/client/src/pages/TenantForm.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+const TenantCheckForm = ({ setCurrentPage }) => {
+  useEffect(() => {
+    setCurrentPage("TenantCheckForm");
+  });
+  return <div></div>;
+};
+
+export default Dashboard;
