ec99d7b8251c1654cba7342bba380d8329146b21
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-02-26 23:55:43 +0000

    Centered form inside page's div

--- moove-admin-app/client/src/pages/TenantForm.jsx
+++ moove-admin-app/client/src/pages/TenantForm.jsx
@@ -5,7 +5,11 @@ const TenantForm = ({ setCurrentPage }) => {
   useEffect(() => {
     setCurrentPage("TenantForm");
   });
-  return <div><TenantCheckForm/></div>;
+  
+  return <div style={{backgroundColor:'white', display: 'flex',  justifyContent:'center', alignItems:'center', width: '900px',
+  height: '400px'}}>
+  <TenantCheckForm/>
+</div>
 };
 
 export default TenantForm;
