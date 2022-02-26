6012a68ed33eb3e62ec14bfb2519f3cecfd9fb07
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-02-26 23:55:29 +0000

    Modified grid layout as per wireframe

--- moove-admin-app/client/src/components/TenantCheckForm.jsx
+++ moove-admin-app/client/src/components/TenantCheckForm.jsx
@@ -17,32 +17,43 @@ export default function TenantCheckForm() {
   const onSubmit = (data) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-
-    <Grid container direction="row">
+    <div>
+    <Grid container direction="row" spacing={2}>
+    <Grid item xs container direction="column" spacing={2}>
       <Grid item xs>
       <Controller
             control={control}
             name="fNameField"
             render={({ field }) => <TextField {...field} label="First Name" />}
           />
+          </Grid>
+          <Grid item xs>
           <Controller
             control={control}
             name="sNameField"
             render={({ field }) => <TextField {...field} label="Surname" />}
           />
+          
+          </Grid>
+          <Grid item xs>
           <Controller
             control={control}
             name="emailField"
             render={({ field }) => <TextField {...field} label="Email" />}
           />
+          </Grid>
+          <Grid item xs>
           <Controller
             control={control}
             name="telephoneField"
             render={({ field }) => (
               <TextField {...field} label="Phone Number" />
             )}
+            
           />
-      </Grid>
+          </Grid>
+          </Grid>
+      <Grid item xs container direction="column" spacing={0}>
       <Grid item xs>
       <Controller
         control={control}
@@ -57,6 +68,8 @@ export default function TenantCheckForm() {
           
         )}
       />
+      </Grid>
+      <Grid item xs>
       <Controller
         control={control}
         name="sIDField"
@@ -70,14 +83,15 @@ export default function TenantCheckForm() {
         )}
       />
       </Grid>
+      </Grid>
       <Grid item xs container direction="column">
         <Grid item xs />
         <Grid item xs><Button type="submit" variant="contained" color="primary">
         Submit
       </Button></Grid>
       </Grid>
-    </Grid>
-
+      </Grid>
+    </div>
       
     </form>
   );
