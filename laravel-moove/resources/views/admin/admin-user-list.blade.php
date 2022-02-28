4be9624b79c829f869c6ee499a20c02fc69b1a25
Liam Newman <newmanl5@cardiff.ac.uk>
2022-02-28 13:41:56 +0000

    Split user list to individual page
    Splits test admin user UI into separate component.

---
+++ laravel-moove/resources/views/admin/admin-user-list.blade.php
@@ -0,0 +1,35 @@
+<h1>User list</h1>
+<table>
+    <thead>
+    <tr>
+        <th>User ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Actions</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($users as $user)
+        <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->role}}</td>
+            <td>
+                @if($user->role != "ADMIN")
+                    <form method="post" action="{{route('admin.setrole', ['id' => $user->id, 'role' => 'ADMIN'])}}">
+                        @csrf
+                        <button type="submit">Make Admin</button>
+                    </form>
+                @else
+                    <form method="post" action="{{route('admin.setrole', ['id' => $user->id, 'role' => 'TENANT'])}}">
+                        @csrf
+                        <button type="submit">Make Tenant</button>
+                    </form>
+                @endif
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
