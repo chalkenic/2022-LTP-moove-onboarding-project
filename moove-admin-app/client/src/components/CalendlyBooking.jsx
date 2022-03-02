1282221cb1be6f28d8def9695014d941e8a7bc4f
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-02 19:07:25 +0000

    Created initial component for Calendly widget

---
+++ moove-admin-app/client/src/components/CalendlyBooking.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { InlineWidget } from "react-calendly";
+
+export default function CalendlyBooking(){
+const App = () => {
+  return (
+    <div className="CalendlyBooking">
+      <InlineWidget url="https://calendly.com/moove__placeholder__url" />
+    </div>
+  );
+};
+};
