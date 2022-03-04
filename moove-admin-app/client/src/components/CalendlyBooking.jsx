74b49a4be6b1d51e9065108ff935e44a32d41ed9
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-04 04:52:46 +0000

    Adjusted calendly booking widget URL

--- moove-admin-app/client/src/components/CalendlyBooking.jsx
+++ moove-admin-app/client/src/components/CalendlyBooking.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { InlineWidget } from "react-calendly";
 
 export default function CalendlyBooking(){
-const App = () => {
   return (
     <div className="CalendlyBooking">
-      <InlineWidget url="https://calendly.com/moove__placeholder__url" />
+      <InlineWidget url="https://calendly.com/moove-booking/30min" />
     </div>
   );
-};
 };
