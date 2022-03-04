efa58fc74cc962f20b793c7be9e7c44aad0601c2
Luke Morcom <luke@morcom.io>
2022-03-04 16:49:31 +0000

    Last minute commit to fix a couple small things ;)

--- laravel-moove/app/Http/Controllers/Admin/AdminController.php
+++ laravel-moove/app/Http/Controllers/Admin/AdminController.php
@@ -13,11 +13,4 @@ class AdminController extends Controller
     public function index() {
         return view('admin.admin-home');
     }
-
-    // Put this in UserConvertController
-    public function setRole($id, $role) {
-        // User::where(['id' => $id])->update(['role' => $role]);
-        return redirect('/');
-    }
-
 }
