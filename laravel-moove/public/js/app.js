6e98d8892639aaa0f87e912f04543afc2d11d45b
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-11 19:24:40 +0000

    Added book appointment page to navbar

--- laravel-moove/public/js/app.js
+++ laravel-moove/public/js/app.js
@@ -12076,6 +12076,13 @@ var navTextTenant = [{
   type: "link"
 }, {
   id: 4,
+  key: "book_appointment",
+  name: "Book Appointment",
+  selected: false,
+  link: "/bookapt",
+  type: "link"
+}, {
+  id: 5,
   key: "page_logout",
   name: "Logout",
   selected: false,
