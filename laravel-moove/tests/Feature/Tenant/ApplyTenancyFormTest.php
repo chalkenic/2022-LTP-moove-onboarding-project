28ed01e797c377f62511e1e347757ce974093cc2
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-11 22:02:29 +0000

    Added test for verifying form page loads

---
+++ laravel-moove/tests/Feature/Tenant/ApplyTenancyFormTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Tenant;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class ApplyTenancyFormTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function tenant_can_see_tenancy_form_page()
+    {
+        $tenant = User::factory()->create(['role' => 'TENANT']);
+        Auth::login($tenant);
+
+        $response = $this->get('/apply-tenancy');
+        $response->assertStatus(200)
+            ->assertViewIs('tenant.tenant-apply-tenancy');
+    }
+
+
+}
