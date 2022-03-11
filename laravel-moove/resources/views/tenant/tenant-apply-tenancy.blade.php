9855dff080e2e3f73516c9f80b7d1de443e72a26
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-11 21:39:42 +0000

    Added new page blade for applying for a tenancy

---
+++ laravel-moove/resources/views/tenant/tenant-apply-tenancy.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.head')
+@section('title')
+<title>moove - book appointment</title>
+@endsection
+@section('content')
+<div>
+    <h1>Apply for Tenancy</h1>
+    <TenancyForm/>
+</div>
+@endsection
