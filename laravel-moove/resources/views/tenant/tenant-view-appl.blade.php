380f088b0ba721d064ffb64c8dfabd751f5a6903
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-13 20:18:33 +0000

    Created page for viewing tenancy application

---
+++ laravel-moove/resources/views/tenant/tenant-view-appl.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.head')
+@section('title')
+<title>moove - view application</title>
+@endsection
+@section('content')
+<div>
+    
+    <h1>View My Application</h1>
+    <TenancyApplicationProgress/>
+</div>
+@endsection
