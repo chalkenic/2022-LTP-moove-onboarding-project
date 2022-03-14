93ea0b90dc18bc397ee05da7eb324b9c331bc40f
Luke Morcom <luke@morcom.io>
2022-03-14 10:20:23 +0000

    Extract methods to controller, tidy web.php

--- laravel-moove/routes/web.php
+++ laravel-moove/routes/web.php
@@ -8,15 +8,11 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\UserConvertController;
+use App\Http\Controllers\AdminApplicationController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\Tenant\ApplicationController;
 use App\Http\Controllers\Tenant\TenantController;
 use App\Http\Controllers\Tenant\TenantAptController;
-use Illuminate\Auth\Events\PasswordReset;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
 use App\Http\Controllers\Landlord\LandlordController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,10 +33,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Tenant routes
 Route::get('/tenant-home', [TenantController::class, 'index'])->name('tenant.home');
-Route::get('/book-appointment', [TenantAptController::class, 'index'])->name('tenant.bookapt');
-
-// Landlord routes
-Route::get('/landlord-home', [TenantController::class, 'index'])->name('landlord.home');
 Route::get('/application', [ApplicationController::class, 'index'])->name('tenant.application');
 Route::get('/start-application', [ApplicationController::class, 'create'])->name('tenant.start-application');
 Route::post('/start-application', [ApplicationController::class, 'store']);
@@ -51,14 +43,10 @@ Route::get('/landlord-home', [LandlordController::class, 'index'])-> name('landl
 Route::get('/landlord-properties', function() {
     return view('landlord.landlord-properties');
 });
+Route::get('/book-appointment', [TenantAptController::class, 'index'])->name('tenant.bookapt');
 
 // Admin routes
 Route::get('/admin-home', [AdminController::class, 'index'])->name('admin.home');
 Route::get('/convert-user', [UserConvertController::class, 'index'])->name('admin.convert-user');
 Route::put('/convert-user', [UserConvertController::class, 'update']);
-Route::get('/admin-tenant-list', function() {
-    return view('admin.admin-tenant-list');
-});
-Route::get('/admin-tenant-application/{id}', function($id) {
-    return view('admin.admin-tenant-application', ['id' => $id]);
-});
+Route::get('/admin-tenant-list', [AdminApplicationController::class, 'index']);
