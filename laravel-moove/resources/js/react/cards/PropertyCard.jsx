25a71c786618d0c698f542cf04b067630dcaf564
Nick <whitenj2@cardiff.ac.uk>
2022-03-16 15:03:57 +0000

    Added property details modal and imported into component card.

--- laravel-moove/resources/js/react/cards/PropertyCard.jsx
+++ laravel-moove/resources/js/react/cards/PropertyCard.jsx
@@ -1,13 +1,17 @@
 import { Grid, Paper } from '@mui/material';
+import PropertyModal from '../components/landlord/PropertyModal';
 
 const PropertyCard = (props) => {
 	return (
+		<div>
+		<PropertyModal/>
 		<Grid item xs={10} sx={{ maxWidth: '350px' }}>
 			<Paper elevation={20} sx={{ height: 300, marginBlock: 5, borderRadius: 5 }}>
                 
 				{props.children}
 			</Paper>
 		</Grid>
+		</div>
 	);
 };
 
---
+++ laravel-moove/resources/js/react/components/landlord/PropertyModal.jsx
@@ -0,0 +1,8 @@
+import { Grid, Typography } from '@mui/material';
+import * as LandlordTexts from '../../assets/texts/LandlordTexts';
+
+const PropertyModal = (props) => {
+return(<div></div>);
+}
+
+export default PropertyModal;
