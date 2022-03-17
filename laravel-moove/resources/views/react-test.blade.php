8c153d44f0a9a5b4d78600268cf72eac182382f4
Luke Morcom <luke@morcom.io>
2022-03-17 21:08:14 +0000

    Add hook, fix linting errors, remove old test stuff

--- laravel-moove/resources/views/react-test.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.head')
-@section('content')
-    <a href="{{route('register')}}" class="rounded-lg font-mono container text-white p-8 flex mx-auto bg-gray-500"><p>Blade element says: {{$message}}</p></a>
-    <div class="rounded-lg font-mono container text-white p-8 flex mx-auto mt-4 bg-gray-500"><TestComponent text="And I am a react component!" /></div>
-@endsection
