b02e97d4224894a8dd24a990d123c3e939c5adba
c1946094 <ShahS58@cardiff.ac.uk>
2022-03-19 17:14:05 +0000

    blade changes

--- laravel-moove/resources/views/tenant/tenant-get-video.blade.php
+++ laravel-moove/resources/views/tenant/tenant-get-video.blade.php
@@ -4,6 +4,19 @@
 @endsection
 @section('content')
 <div>
-
+<h2 class="flex justify-center font-medium leading-tight text-4xl mt-0 mb-2">
+    User Conversion
+</h2>
 </div>
+<div class="flex justify-center ">
+    <p>
+        Enter an email address below. If it belongs to a moove account
+        that doesn't have an open application, it will be converted to a
+        moove admin account when you click convert.
+    </p>
+</div>
+<!-- <div>
+<form action={{ route('/get-video/{title}') }} method="POST" class="mt-4">
+        @csrf
+</div> -->
 @endsection
