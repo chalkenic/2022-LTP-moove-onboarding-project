ec0a22c69b4d5f0622d4332fb19fe9b230bb8a7c
Luke Morcom <luke@morcom.io>
2022-03-21 21:54:52 +0000

    Add fix command, fix auto-fixable lints

--- laravel-moove/resources/js/react/components/helpers/helper.js
+++ laravel-moove/resources/js/react/components/helpers/helper.js
@@ -1,5 +1,7 @@
 // Reusable functions within app that can be used for functionality.
 
-export function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+export function capitalizeFirstLetter (string) {
+
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
 }
