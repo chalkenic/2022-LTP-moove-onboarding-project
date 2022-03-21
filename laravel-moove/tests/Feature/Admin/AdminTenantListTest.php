9dc983b2094405c2059c71d2b807008b94157389
Liam Newman <newmanl5@cardiff.ac.uk>
2022-03-21 10:42:46 +0000

    Add tests to tenant list
    Add tests to confirm permissions when accessing the tenant list.

---
+++ laravel-moove/tests/Feature/Admin/AdminTenantListTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Application;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AdminTenantListTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function admin_can_see_tenant_list() {
+        $admin = User::factory()->create(['role' => 'ADMIN']);
+        Auth::login($admin);
+
+        $response = $this->get('/admin-tenants');
+        $response->assertStatus(200)
+            ->assertViewIs('admin.admin-tenant-list');
+    }
+
+    /**
+     * @test
+     */
+    public function tenant_cannot_see_tenant_list()
+    {
+        $tenant = User::factory()->create(['role' => 'TENANT']);
+        Auth::login($tenant);
+
+        $response = $this->get('/admin-tenants');
+        $response->assertStatus(302);
+    }
+}
