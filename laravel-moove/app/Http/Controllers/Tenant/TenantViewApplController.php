5e96a22b99652f34ae4e3b18ad7f11ff01f1de43
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-26 01:53:58 +0000

    Added the ability to get fellow tenants from property ID

--- laravel-moove/app/Http/Controllers/Tenant/TenantViewApplController.php
+++ laravel-moove/app/Http/Controllers/Tenant/TenantViewApplController.php
@@ -15,4 +15,23 @@ class TenantViewApplController extends Controller
     public function index() {
         return view('tenant.tenant-view-appl');
     }
+
+    public function fellowtenants($id){
+        if(Property::where('id', $id)->exists()) { 
+            $tenants = User::whereRelation('tenancy', 'property_id', $id)->get();
+            return response()->json(
+                [
+                    'fellowtenants' => $tenants
+                ],
+                200
+            );
+
+        } else {
+            return response()->json([
+                'status'=>200,
+                'message'=>`no housemates at property of ID `.$id,
+                            
+            ]);
+        }
+    }
 }
