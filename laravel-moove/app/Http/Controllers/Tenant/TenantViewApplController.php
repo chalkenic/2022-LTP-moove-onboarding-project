7d1191efcf41a46c4e0347e7a360ba56fa64d4ab
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-27 00:31:00 +0000

    Fixed error with missing dependency and added tenant contract variable

--- laravel-moove/app/Http/Controllers/Tenant/TenantViewApplController.php
+++ laravel-moove/app/Http/Controllers/Tenant/TenantViewApplController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use JavaScript;
+use App\Models\User;
+
 use App\Models\Property;
 use App\Models\Tenancy;
 
@@ -19,15 +21,17 @@ class TenantViewApplController extends Controller
     public function index() {
         try{
             $ownPropertyId = Tenancy::where('user_id',Auth::id())->first()->property_id;
-            $tenants = User::whereRelation('tenancy', 'property_id', $ownPropertyId)->get();
+            $tenantsContracts = Tenancy::where('property_id',$ownPropertyId)->get();
+
             JavaScript::put([
                 'property' => Property::where('id', $ownPropertyId)->first(),
-                'tenants' => $tenants;
+                'tenants' => $tenantsContracts
             ]);
         }
         catch (\Throwable $e){
             JavaScript::put([
                 'noTenancy' => true,
+                'errorMsg' => $e->getMessage()
             ]);
         }
 
