7428a14dd7f5170a7e7119f9d2f19839ce6ba813
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-27 02:01:20 +0100

    Testing switch/case in Laravel helper function

--- laravel-moove/app/Http/Helpers.php
+++ laravel-moove/app/Http/Helpers.php
@@ -1,5 +1,14 @@
 if (! function_exists('convert_status_id')) {
     function convert_status_id($id) {
-        
+        switch($id)
+            case(1):
+                return '';
+                break;
+            case(2):
+                return 'pe';
+                break;
+            case(3):
+                return 'po';
+                break;
     }
 }
