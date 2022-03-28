e0ca0cdb5603568f56faa53502ba7c3c0dd3bcc0
Luke Morcom <luke@morcom.io>
2022-03-28 14:58:01 +0100

    Small UI changes, remove random logout button from tenant view

--- laravel-moove/resources/views/tenant/tenant-home.blade.php
+++ laravel-moove/resources/views/tenant/tenant-home.blade.php
@@ -4,21 +4,36 @@
 @endsection
 @section('content')
 <div>
+    <h1 class="font-medium leading-tight text-3xl mt-0 mb-2">
+        Welcome to moove, {{ auth()->user()->name }}
+    </h1>
+    
+    <p class="flex mb-1 mt-8">
+        @if($hasStartedApplication && $applicationStatus == 0)
+            It looks like you already have an application open. Don't worry,
+            we will get it reviewed as soon as possible and you'll soon
+            be on your way to mooving with us!
+        @elseif(!$hasStartedApplication)
+            We're glad you're here! We'll need to manually approve you to
+            rent with moove before you can go any further. Click the button
+            below to begin!
+        @endif
+    </p>
     @if($hasStartedApplication && $applicationStatus == 0)
-        Continue your application process. <a href={{route('tenant.application')}}>Click here</a>
-    @elseif(!$hasStartedApplication)
-        Begin your application process. <a href={{route('tenant.start-application')}}>Click here</a>
+        <p class="flex mb-1">
+            Need to add documents to your application?
+        </p>
     @endif
-
+    @if($hasStartedApplication && $applicationStatus == 0)
+        <a href="{{route('tenant.application')}}"><button class="bg-blue-500 hover:bg-blue-400 transition-colors rounded-[8px] px-[15px] py-[4px] text-white focus:ring-2 ring-blue-500">Let's go!</button></a>
+    @elseif(!$hasStartedApplication)
+        <a href="{{route('tenant.start-application')}}"><button class="bg-blue-500 hover:bg-blue-400 transition-colors rounded-[8px] px-[15px] py-[4px] text-white focus:ring-2 ring-blue-500">Let's go!</button></a>
+    @endif
     @if($applicationStatus == 1)
         Great news! Your application has been approved and you are ready to get mooving!
     @elseif($applicationStatus == 2)
         Unfortunately, your application was denied.
     @endif
 </div>
-    <form method="POST" class="inline" action="{{route('logout')}}">
-        @csrf
-        <button class="bg-red-500 rounded text-white p-2">Logout</button>
-    </form>
-</div>
+
 @endsection
