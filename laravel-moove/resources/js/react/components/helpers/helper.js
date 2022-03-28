203c2a1c6a6dfed6baf0607307a403c371967e57
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-28 18:48:52 +0100

    Adapted Laravel helper function to convert status ID into JS helper

--- laravel-moove/resources/js/react/components/helpers/helper.js
+++ laravel-moove/resources/js/react/components/helpers/helper.js
@@ -5,3 +5,22 @@ export function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 
 }
+
+            //Status IDs
+            // 0 - Awaiting tenancy documents
+            // 1 - Awaiting approval
+            // 2 - Application Approved
+            // 3 - Application Denied
+
+export function convertStatusId(id) {
+    switch (id) {
+        case 0:
+          return "Awaiting tenancy documents";
+        case 1:
+          return "Awaiting approval";
+        case 2:
+          return "Application Approved";
+        case 3:
+          return "Application Denied";
+    }
+}
