2045596ca0388f5374009a3220890e1a0081859d
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-28 18:36:02 +0100

    Added check for if helper function exists

--- laravel-moove/app/Helpers.php
+++ laravel-moove/app/Helpers.php
@@ -1,20 +1,16 @@
 <?php
 
-class Helpers
-{
-    public static function convert_status_id(int $id)
+if(!function_exists('convert_status_id')){
+function convert_status_id(int $id)
     {
         
         switch($id):
             case(1):
                 return '';
-                break;
             case(2):
                 return 'pe';
-                break;
             case(3):
                 return 'po';
-                break;
             default:
                 return 'error';
             endswitch;
