96f5c360f1ed2ae0dbad4994bd91b480c5af08bb
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-29 04:06:16 +0100

    Added selected tenant status into children of modal component

--- laravel-moove/resources/js/react/components/tenant/TenancyApplicationProgress.jsx
+++ laravel-moove/resources/js/react/components/tenant/TenancyApplicationProgress.jsx
@@ -97,10 +97,7 @@ const TenancyApplicationProgress = () => {
     </div> }
 
     <Modal onClose={handleClose} show={show} tenantName={selectedTenantName}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
-          deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus non
-          fuga omnis a sed impedit explicabo accusantium nihil doloremque
-          consequuntur.
+          {selectedTenantStatus}
         </Modal>
     </div>
   );
