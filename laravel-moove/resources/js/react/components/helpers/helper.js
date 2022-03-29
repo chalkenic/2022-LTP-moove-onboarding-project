a3c6adf333e524e2f91d2d95d4f024efcbc12bb8
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-29 01:43:21 +0100

    Misc fixes

--- laravel-moove/resources/js/react/components/helpers/helper.js
+++ laravel-moove/resources/js/react/components/helpers/helper.js
@@ -6,21 +6,27 @@ export function capitalizeFirstLetter(string) {
 
 }
 
-            //Status IDs
-            // 0 - Awaiting tenancy documents
-            // 1 - Awaiting approval
-            // 2 - Application Approved
-            // 3 - Application Denied
+/*
+ * Status IDs
+ * 0 - Awaiting tenancy documents
+ * 1 - Awaiting approval
+ * 2 - Application Approved
+ * 3 - Application Denied
+ */
 
 export function convertStatusId(id) {
+
     switch (id) {
-        case 0:
-          return "Awaiting tenancy documents";
-        case 1:
-          return "Awaiting approval";
-        case 2:
-          return "Application Approved";
-        case 3:
-          return "Application Denied";
+
+    case 0:
+        return "Awaiting tenancy documents";
+    case 1:
+        return "Awaiting approval";
+    case 2:
+        return "Application Approved";
+    case 3:
+        return "Application Denied";
+
     }
+
 }
