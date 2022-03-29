b50a274d8d62c88cdae86cb4f0f531b532cf5d80
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-29 03:52:52 +0100

    Fixed bug with incorrect variable passed into modal

--- laravel-moove/resources/js/react/components/tenant/TenancyApplicationProgress.jsx
+++ laravel-moove/resources/js/react/components/tenant/TenancyApplicationProgress.jsx
@@ -92,7 +92,7 @@ const TenancyApplicationProgress = () => {
       </Alert>
     </div> }
 
-    <Modal onClose={handleClose} show={show} tenantName={tenantName}>
+    <Modal onClose={handleClose} show={show} tenantName={selectedTenantName}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
           deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus non
           fuga omnis a sed impedit explicabo accusantium nihil doloremque
