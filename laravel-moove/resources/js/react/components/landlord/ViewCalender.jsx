745b8e930a09a007c362c4d8860398ce2d01dc16
c1946094 <ShahS58@cardiff.ac.uk>
2022-03-29 18:04:32 +0100

    able to send api request to calendly using axios

--- laravel-moove/resources/js/react/components/landlord/ViewCalender.jsx
+++ laravel-moove/resources/js/react/components/landlord/ViewCalender.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { InlineWidget } from 'react-calendly';
 import axios from 'axios';
 
 
 
-const handleApproval = () => {
+const handleGetUser = () => {
 
-      axios.get('https://api.calendly.com/scheduled_events', {
-      }).then((res) => {
-        
-      }).catch((err) => {
-        setError(err.message)
-      })
+    const url = 'https://api.calendly.com/users/me'
+    const token = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjQ4NTcxNTcyLCJqdGkiOiIyNGI1NjViMy05MzhlLTQzMmUtODBiNy1jMzI3M2JjMzM1YmIiLCJ1c2VyX3V1aWQiOiJhYjJlNmYyNy1hNTZlLTQzZWItYjM1ZC1jNWViYWRhOTBiMzEifQ.sdN0HcP8ZGqzeImxyFtm6ss665Nvhw4q7RYVnZn_tg0'
+
+
+    axios.get(url, { 'headers': { 'Authorization': token } })
+  .then((response => {
+    console.log(response.data);
+  }))
   }
 
 
 export default function ViewCalender() {
 
+    useEffect(() => {
+        handleGetUser()
+        
+    })
+
     return(
-        <>
-        <div className="ViewCalender">
-          <InlineWidget url="https://calendly.com/moove-landlord" />
+        <div>
+
         </div>
-        </>
     )
 }
