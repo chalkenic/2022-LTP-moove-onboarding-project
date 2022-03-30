98cdb7b9c2f2770cd837b5e66a13f7b6b315570b
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-30 23:39:47 +0100

    Started working on store function for invite landlord controller

--- laravel-moove/app/Http/Controllers/Admin/AdminInvLandlordController.php
+++ laravel-moove/app/Http/Controllers/Admin/AdminInvLandlordController.php
@@ -13,4 +13,14 @@ class AdminInvLandlordController extends Controller
     public function index() {
         return view('admin.admin-invite-landlord');
     }
+
+    public function store(Request $request) {
+        $request->validate(['email' => 'required|email']);
+        
+        Password::sendResetLink((
+            $request->only('email')
+        ));
+
+        return back()->with('status', 'Email containing a link to sign up as a landlord has been sent to the email specified.');
+    }
 }
