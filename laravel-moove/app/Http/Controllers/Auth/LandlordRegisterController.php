85ac6b063ecb6d99ff1a3e5837fd08ce53bee381
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-30 20:46:20 +0100

    Fixed placeholder info inside landlord register controller - model complete

--- laravel-moove/app/Http/Controllers/Auth/LandlordRegisterController.php
+++ laravel-moove/app/Http/Controllers/Auth/LandlordRegisterController.php
@@ -3,26 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
 
 class LandlordRegisterController extends Controller
 {
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware(['guest']);
     }
 
     public function index() {
-        return view('auth.forgot-password');
+        return view('auth.landlord-register');
     }
 
     public function store(Request $request) {
-        $request->validate(['email' => 'required|email']);
-        
-        Password::sendResetLink((
-            $request->only('email')
-        ));
+        $this->validate($request, [
+            'name' => 'required|alpha|max:50',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|confirmed'
+        ]);
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => 'LANDLORD',
+            'password' => Hash::make($request->password)
+        ]);
 
-        return back()->with('status', 'If there is a moove account with that email, we just sent reset instructions to it.');
+        auth()->attempt($request->only('email', 'password'));
+        return redirect()->route('landlord.home');
     }
 }
