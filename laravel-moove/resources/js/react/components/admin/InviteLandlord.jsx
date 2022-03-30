a537d7c4dc3d887942b7e2047aca7edbe5a91cf4
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-31 00:18:01 +0100

    Added email input into Axios request for invite landlord component

--- laravel-moove/resources/js/react/components/admin/InviteLandlord.jsx
+++ laravel-moove/resources/js/react/components/admin/InviteLandlord.jsx
@@ -3,16 +3,29 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import FormHelperText from '@mui/material/FormHelperText';
 import Button from '@mui/material/Button';
 
+const createLandlordInv = () => {
+    axios
+    .post("/landlord-sign-tenancy", {
+        email: document.getElementById('emailField').value,
+    })
+    .then(() => {
+      postPost();
+      enableEdit();
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
+  }
 
 const InviteLandlordComponent = ({}) => {
     return (
         <div>
             <h1>Invite Landlord to Moove and Automatically Setup as Landlord</h1><br></br>
             <FormControl sx={{ width: '25ch' }}>
-  <OutlinedInput placeholder="Landlord Email" />
+  <OutlinedInput class="emailField" placeholder="Landlord Email" />
   <FormHelperText>Email (verify before submitting!)</FormHelperText>
 </FormControl><br></br><br></br>
-<Button variant="outlined">Send Email</Button>
+<Button variant="outlined" onClick={createLandlordInv}>Send Email</Button>
 
         </div>
     )
