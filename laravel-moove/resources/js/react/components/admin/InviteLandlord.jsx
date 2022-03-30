b4efd0565873e1844cdbec944984dc9f18aa3a49
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-31 00:30:33 +0100

    Fixed bug with adding input field data to POST request

--- laravel-moove/resources/js/react/components/admin/InviteLandlord.jsx
+++ laravel-moove/resources/js/react/components/admin/InviteLandlord.jsx
@@ -2,11 +2,13 @@ import FormControl from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormHelperText from '@mui/material/FormHelperText';
 import Button from '@mui/material/Button';
+import React from 'react';
+
 
 const createLandlordInv = () => {
     axios
     .post("/landlord-sign-tenancy", {
-        email: document.getElementById('emailField').value,
+        email: document.getElementById("emailField").value,
     })
     .then(() => {
         console.log("send");
@@ -21,7 +23,7 @@ const InviteLandlordComponent = ({}) => {
         <div>
             <h1>Invite Landlord to Moove and Automatically Setup as Landlord</h1><br></br>
             <FormControl sx={{ width: '25ch' }}>
-  <OutlinedInput class="emailField" placeholder="Landlord Email" />
+  <OutlinedInput id="emailField" placeholder="Landlord Email" />
   <FormHelperText>Email (verify before submitting!)</FormHelperText>
 </FormControl><br></br><br></br>
 <Button variant="outlined" onClick={createLandlordInv}>Send Email</Button>
