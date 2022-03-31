986d83dcc9ffae4622bc694176b9b154a2a1c319
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-31 02:22:39 +0100

    Added debugging print statement

--- laravel-moove/app/Http/Controllers/Auth/LandlordRegisterController.php
+++ laravel-moove/app/Http/Controllers/Auth/LandlordRegisterController.php
@@ -32,6 +32,7 @@ class LandlordRegisterController extends Controller
             'password' => Hash::make($request->password)
         ]);
 
+        print("hi");
         auth()->attempt($request->only('email', 'password'));
         return redirect()->route('landlord.home');
     }
