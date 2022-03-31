5184a256c407469cc3a97a72ca8a41c5247fc0b6
Ronan Manning <ManningR4@cardiff.ac.uk>
2022-03-31 16:09:05 +0100

    Added success and error messages upon form submission.

--- laravel-moove/resources/js/react/components/admin/InviteLandlord.jsx
+++ laravel-moove/resources/js/react/components/admin/InviteLandlord.jsx
@@ -1,26 +1,34 @@
 import FormControl from '@mui/material/FormControl';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormHelperText from '@mui/material/FormHelperText';
-import Button from '@mui/material/Button';
+import {Button, Alert} from '@mui/material';
 import React from 'react';
 
 
-const createLandlordInv = () => {
-    axios
-    .post("/admin-invite-landlord", {
-        email: document.getElementById("emailField").value,
-    })
-    .then(() => {
-        console.log("send");
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
-  }
 
 const InviteLandlordComponent = ({}) => {
+    const [showSuccess, setShowSuccess] = React.useState(false);
+    const [showError, setShowError] = React.useState(false);
+    const postPost = () => setShowSuccess(true);    
+
+    const createLandlordInv = () => {
+        axios
+        .post("/admin-invite-landlord", {
+            email: document.getElementById("emailField").value,
+        })
+        .then(() => {
+            postPost();
+        })
+        .catch((error) => {
+            console.log(error.message);
+            setShowError(true);
+        });
+      }
+    
     return (
         <div>
+            { showSuccess ? <div><Alert severity="success">Email successfully sent, awaiting their response!</Alert><br></br></div> : null }
+            { showError ? <div><Alert severity="error">Email failed to send, an error has occurred! Please check your input and try again.</Alert><br></br></div> : null }
             <h1>Invite Landlord to Moove and Automatically Setup as Landlord</h1><br></br>
             <FormControl sx={{ width: '25ch' }}>
   <OutlinedInput id="emailField" placeholder="Landlord Email" />
